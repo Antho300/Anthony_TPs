6c79277af420ea25ee999f330f087c27d2d4b11a
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-04 15:26:18 +0200

    TP1_ex1

---
+++ manipNombresInt_NAVARRO/src/manipnombresint_navarro/ManipNombresInt_NAVARRO.java
@@ -0,0 +1,27 @@
+// 04/10/2021 Exercice 1 TP1 : Saisie et manipulation de nombres
+
+package manipnombresint_navarro;
+
+/**
+ *
+ * @author Anthony
+ */
+public class ManipNombresInt_NAVARRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+       
+        
+
+
+
+// TODO code application logic here
+    }
+    
+}
