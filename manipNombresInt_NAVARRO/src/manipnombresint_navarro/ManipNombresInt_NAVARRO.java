aa2acbf055c9e133666da050a8b5f8773310b9fb
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-04 15:56:05 +0200

    TP1_ex1

--- manipNombresInt_NAVARRO/src/manipnombresint_navarro/ManipNombresInt_NAVARRO.java
+++ manipNombresInt_NAVARRO/src/manipnombresint_navarro/ManipNombresInt_NAVARRO.java
@@ -2,6 +2,8 @@
 
 package manipnombresint_navarro;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Anthony
@@ -13,9 +15,31 @@ public class ManipNombresInt_NAVARRO {
      */
     public static void main(String[] args) {
         
+        int nb1;
+        int nb2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer un nombre :");
+        nb1 = sc.nextInt();
+        System.out.println("\n Entrer un autre nombre :");
+        nb2 = sc.nextInt();
+        
+        System.out.println("Voici les nombres choisis :");
+        System.out.println(nb1 + " " + nb2);
+        System.out.println("Voici leur somme :");
+        System.out.println(nb1+nb2);
+        System.out.println("Voici leur différence :");
+        System.out.println(nb1-nb2);
+        System.out.println("Voici leur produit :");
+        System.out.println(nb1*nb2);
+        System.out.println("Voici le quotient :");
+        System.out.println(nb1/nb2);
+        System.out.println("Voici le reste :");
+        System.out.println(nb1%nb2);
         
         
         
+        
+               
        
         
 
