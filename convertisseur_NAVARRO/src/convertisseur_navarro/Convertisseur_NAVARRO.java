3d21d0a8142709fcf98c195ccd46438c11f7a859
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-04 16:02:28 +0200

    TP1_ex2

---
+++ convertisseur_NAVARRO/src/convertisseur_navarro/Convertisseur_NAVARRO.java
@@ -0,0 +1,31 @@
+// 04/10/2021 TP1 ex2 : Un convertisseur de températures
+
+package convertisseur_navarro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Convertisseur_NAVARRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double nb1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer un nombre :");
+        nb1 = sc.nextDouble();
+        System.out.println(nb1);
+        
+        
+        
+        
+       
+    
+    }
+    
+}
