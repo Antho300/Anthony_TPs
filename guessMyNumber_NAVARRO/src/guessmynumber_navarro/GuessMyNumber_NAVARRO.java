ad09dda75bc0b1ca78a4d160506c2f75fb0e3b09
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-05 00:20:47 +0200

    TP1_ex3

--- guessMyNumber_NAVARRO/src/guessmynumber_navarro/GuessMyNumber_NAVARRO.java
+++ guessMyNumber_NAVARRO/src/guessmynumber_navarro/GuessMyNumber_NAVARRO.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+// 04/10/2021 TP1 ex3 GuessMyNumber
 package guessmynumber_navarro;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +14,42 @@ public class GuessMyNumber_NAVARRO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Random generateurAleat = new Random();
+        //int n = generateurAleat.nextInt(100); // n prend une valeur aléatoire inférieure ou égale à 100
+        //System.out.println(n);
+        //n = generateurAleat.nextInt(100);
+        //System.out.println(n);
+        //n = generateurAleat.nextInt(100);
+        //System.out.println(n);
+        //n = generateurAleat.nextInt(100);
+        //System.out.println(n);
+        //n = generateurAleat.nextInt(100);
+        //System.out.println(n);
+        
+        int n = generateurAleat.nextInt(100);
+        int utilisateur = 101;
+        int tentative = 0;
+        while (utilisateur != n){
+            tentative += 1;
+            System.out.println("Saisissez un nombre entre 0 et 100 uniquement.");
+            Scanner sc = new Scanner(System.in);
+            utilisateur = sc.nextInt();
+            if (utilisateur > n){
+                System.out.println("Trop grand.");
+            } else if (utilisateur < n) {
+                System.out.println("Trop petit.");
+            }
+        }
+        
+        if (utilisateur == n){
+            System.out.println("Egal au nombre généré aléatoirement.");
+            System.out.println("Le nombre de tentatives est de : " + tentative);
+        }
+        
+        
+        
+        
     }
     
 }
