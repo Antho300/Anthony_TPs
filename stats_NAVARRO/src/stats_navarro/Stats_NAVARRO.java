2f766673fc50d2f9e24b5393bf9eb943a91008e7
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-06 18:33:17 +0200

    TP1_ex4

--- stats_NAVARRO/src/stats_navarro/Stats_NAVARRO.java
+++ stats_NAVARRO/src/stats_navarro/Stats_NAVARRO.java
@@ -1,6 +1,7 @@
 // TP 1, ex 4 (Statistiques), 06/10/2021
 package stats_navarro;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -16,13 +17,24 @@ public class Stats_NAVARRO {
         
         int de [];
         de = new int[6];
-        int m;
+        double m;
+        int n;
+        Random generateurAleat = new Random();
         
         System.out.println("Rentrez un entier");
         Scanner sc = new Scanner (System.in);
         m = sc.nextInt();
         
+        for (int i = 0 ; i < m ; i++){
+            n = generateurAleat.nextInt(6);
+            de[n] += 1;
+        }
         
+        System.out.println("Voici le pourcentage de tirage de chaque chiffre :");
+        for (int j =0 ; j < 6 ; j++){
+            int l = j+1;
+            System.out.println("Le "+ l +" = "+ de[j] * 100/m+"%.");
+        }
         
         
         
