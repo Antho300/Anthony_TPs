9839a5e33dcdda6c68dacb350e54c29185e534ff
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-06 18:05:20 +0200

    TP1_ex4

---
+++ stats_NAVARRO/src/stats_navarro/Stats_NAVARRO.java
@@ -0,0 +1,35 @@
+// TP 1, ex 4 (Statistiques), 06/10/2021
+package stats_navarro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Stats_NAVARRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int de [];
+        de = new int[6];
+        int m;
+        
+        System.out.println("Rentrez un entier");
+        Scanner sc = new Scanner (System.in);
+        m = sc.nextInt();
+        
+        
+        
+        
+        
+        
+        
+        
+      
+    }
+    
+}
