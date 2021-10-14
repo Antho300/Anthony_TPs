ad74aa8f866adf99abf9b5437e500d6a39dbecf4
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-14 19:28:07 +0200

    TP2_ex2

--- TP2_convertisseurObjet_NAVARRO/src/tp2_convertisseurobjet_navarro/Convertisseur.java
+++ TP2_convertisseurObjet_NAVARRO/src/tp2_convertisseurobjet_navarro/Convertisseur.java
@@ -14,17 +14,19 @@ public class Convertisseur {
     public void CelciusVersKelvin (double Celcius) {
         double TempCelcius = Celcius;
         double Tempkelvin = Celcius + 273.15;
+        System.out.println(TempCelcius + " °C est égal à " + Tempkelvin + " K.");
     }
     
     public void KelvinVersCelcius (double Kelvin){
         double TempKelvin = Kelvin;
         double TempCelcius = TempKelvin - 273.15;
-        
+        System.out.println(TempKelvin + " K est égal à " + TempCelcius + " °C.");
     }
     
     public void FarenheightVersCelcius (double Farenheight){
         double TempFarenheight = Farenheight;
         double TempsCelcius = (TempFarenheight - 32) * 5/9;
+        System.out.println(TempFarenheight + " °F est égal à " + TempsCelcius + " °C.");
     
     }
     
@@ -32,20 +34,31 @@ public class Convertisseur {
     public void CelciusVersFarenheight (double Celcius) {
         double TempCelcius = Celcius;
         double Farenheit = TempCelcius * 9/5 + 32;
+        System.out.println(TempCelcius + " °C est égal à " + Farenheit + " °F.");
     }
     
     
     public void FarenheightVersKelvin (double Farenheight){
         double TempFarenheight = Farenheight;
         double kelvin = (TempFarenheight - 32) / 1.8 + 273.15 ;
+        System.out.println(TempFarenheight + " °F est égal à " + kelvin + " K.");
     }
     
     
     public void KelvinVersFarenheight (double Kelvin){
         double TempKelvin = Kelvin;
         double Farenheit = (TempKelvin - 273.15) * 1.8 + 32 ;
-        
+        System.out.println(TempKelvin + " K est égal à " + Farenheit + " °F.");
     }
+    
+    @Override
+    public String toString () {
+        return "nb de conversions"+ nbConversions;
+    }
+
+    
+    
+    
         
     
         
--- TP2_convertisseurObjet_NAVARRO/src/tp2_convertisseurobjet_navarro/TP2_convertisseurObjet_NAVARRO.java
+++ TP2_convertisseurObjet_NAVARRO/src/tp2_convertisseurobjet_navarro/TP2_convertisseurObjet_NAVARRO.java
@@ -1,6 +1,8 @@
 // Ex 2 TP2, convertisseur mais avec l'utilisation objet 14/10/2021
 package tp2_convertisseurobjet_navarro;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Anthony
@@ -12,10 +14,43 @@ public class TP2_convertisseurObjet_NAVARRO {
      */
     public static void main(String[] args) {
         
+        double nb1;
+        int utilisateur;
+        
+        Convertisseur convertisseur = new Convertisseur();
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nBonjour, saisissez une valeur :");
+        nb1 = sc.nextDouble();
+        System.out.println("\nSaisissez la conversion que vous souhaitez effectuer :\n1) De Celcius vers Kelvin\n2) De Kelvin vers Celcius\n3) De Fahrenheit vers Celcius\n4) De Celcius vers Fahrenheit\n5) De Kelvin vers Fahrenheit\n6) De Fahrenheit vers Kelvin");
+        utilisateur = sc.nextInt();
+        System.out.println("Voici votre choix : " + utilisateur);
         
         
         
+        if (utilisateur == 1){
+            convertisseur.CelciusVersKelvin(nb1);
+            
+        } else if (utilisateur == 2){
+            convertisseur.KelvinVersCelcius(nb1);
+            
+        } else if (utilisateur == 3){
+            convertisseur.FarenheightVersCelcius(nb1);
+            
+        } else if (utilisateur == 4){
+            convertisseur.CelciusVersFarenheight(nb1);
+            
+        } else if (utilisateur == 5){
+            convertisseur.KelvinVersFarenheight(nb1);
+            
+        } else if (utilisateur == 6){
+            convertisseur.FarenheightVersKelvin(nb1);
+            
+        } else {
+            System.out.println("Désolé, je n'ai pas compris...");
+        }
         
+        convertisseur.Convertisseur() += 1;
         
         
         
