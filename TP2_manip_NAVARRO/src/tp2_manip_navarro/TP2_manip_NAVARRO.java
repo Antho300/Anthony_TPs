434dd706cc42fa6256c56255666d5372fed5d89d
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-17 19:16:10 +0200

    TP2_ex3

--- TP2_manip_NAVARRO/src/tp2_manip_navarro/TP2_manip_NAVARRO.java
+++ TP2_manip_NAVARRO/src/tp2_manip_navarro/TP2_manip_NAVARRO.java
@@ -14,7 +14,18 @@ public class TP2_manip_NAVARRO {
      */
     public static void main(String[] args) {
         
+        Tartiflette assiette1 = new Tartiflette (500);
+        Tartiflette assiette2 = new Tartiflette (600);
         
+        Tartiflette assiette3 = assiette2;
+        assiette2 = assiette1;
+        assiette1 = assiette3;
+        
+        // Moussaka assiette4 = assiette1; // FAUX
+        // Moussaka assiette5 = new Tartiflette(); // FAUX
+        
+        System.out.println("nb de calories de Assiette 2 : " + (assiette2.nbCalories + 2));
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
         
         
         
