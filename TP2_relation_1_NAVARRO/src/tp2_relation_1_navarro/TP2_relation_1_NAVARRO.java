79849caa34d78ac71c98d8739613ebb3452a634e
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-22 15:32:00 +0200

    TP2_ex4

---
+++ TP2_relation_1_NAVARRO/src/tp2_relation_1_navarro/TP2_relation_1_NAVARRO.java
@@ -0,0 +1,17 @@
+// TP 2, Ex 4, 22/10/2021
+package tp2_relation_1_navarro;
+
+/**
+ *
+ * @author Anthony
+ */
+public class TP2_relation_1_NAVARRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
