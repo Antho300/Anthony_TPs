9b875d5facc3509521542abe0f74b04454c57bb1
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-10-22 15:23:43 +0200

    TP2_ex3

--- TP2_manip_NAVARRO/src/tp2_manip_navarro/TP2_manip_NAVARRO.java
+++ TP2_manip_NAVARRO/src/tp2_manip_navarro/TP2_manip_NAVARRO.java
@@ -24,6 +24,18 @@ public class TP2_manip_NAVARRO {
         // Moussaka assiette4 = assiette1; // FAUX
         // Moussaka assiette5 = new Tartiflette(); // FAUX
         
+        Moussaka [] tab = new Moussaka [10];
+        tab[0] = new Moussaka(1);
+        tab[1] = new Moussaka(1);
+        tab[2] = new Moussaka(1);
+        tab[3] = new Moussaka(1);
+        tab[4] = new Moussaka(1);
+        tab[5] = new Moussaka(1);
+        tab[6] = new Moussaka(1);
+        tab[7] = new Moussaka(1);
+        tab[8] = new Moussaka(1);
+        tab[9] = new Moussaka(1);
+        
         System.out.println("nb de calories de Assiette 2 : " + (assiette2.nbCalories + 2));
         System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
         
