6358db5760a252adfab45ac46520fb714d4f3fad
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-11-15 12:57:40 +0100

    SP4

--- SP4_console_NAVARRO/src/sp4_console_navarro/Partie.java
+++ SP4_console_NAVARRO/src/sp4_console_navarro/Partie.java
@@ -19,10 +19,11 @@ public class Partie {
     
     public void initialiserPartie(){
       grilleJeu = new Grille();
-      Jeton jeton1 = new Jeton("Rouge");
-      Jeton jeton2 = new Jeton("Jaune");
+      
       
       for (int i = 0; i < 21; i++){
+          Jeton jeton1 = new Jeton("Rouge");
+          Jeton jeton2 = new Jeton("Jaune");
           ListeJoueurs[0].ajouterJeton(jeton1);
           ListeJoueurs[1].ajouterJeton(jeton2);
         
