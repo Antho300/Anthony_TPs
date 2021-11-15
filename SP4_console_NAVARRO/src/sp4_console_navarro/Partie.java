a5bc5f9ba2c92074e9c1b4c50dfada0a4c2b2a21
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-11-15 11:02:51 +0100

    SP4

---
+++ SP4_console_NAVARRO/src/sp4_console_navarro/Partie.java
@@ -0,0 +1,16 @@
+// Classe Partie
+package sp4_console_navarro;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Partie {
+    
+    Joueur [] ListeJoueurs = new Joueur [2];
+    
+    
+    
+    
+    
+}
