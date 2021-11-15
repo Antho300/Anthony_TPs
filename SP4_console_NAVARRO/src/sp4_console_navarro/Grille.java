503b0652e1060250842b3ce75ba1d74d019f155c
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-11-15 10:57:14 +0100

    SP4

--- SP4_console_NAVARRO/src/sp4_console_navarro/Grille.java
+++ SP4_console_NAVARRO/src/sp4_console_navarro/Grille.java
@@ -8,8 +8,7 @@ package sp4_console_navarro;
 public class Grille {
     
     Cellule [][] CellulesJeu = new Cellule [6][7];
-    Cellule cellule = new Cellule();
-    
+        
     public Grille(){
         
     }
@@ -20,12 +19,11 @@ public class Grille {
         Jeton j = m;
         
         for (int i = 0; i < 6; i++){
-            if (i == 5 && CellulesJeu [i][colonne] != null){
+            if (i == 5 && CellulesJeu [i][colonne].jetonCourant != null){
                 return false;
                 
-            } else if (CellulesJeu [i][colonne] == null){
-                cellule.affecterJeton(j);
-                CellulesJeu [i][colonne] = cellule;
+            } else if (CellulesJeu [i][colonne].jetonCourant == null){
+                CellulesJeu [i][colonne].affecterJeton(j);
                 return true;
                 
             }else{
@@ -33,26 +31,26 @@ public class Grille {
             }
             
         }
-        
+       return false; 
     }
     
     public boolean etreRemplie(){
         for (int i = 0; i < 6; i++){
             for (int j = 0; j < 7; j++){
-                if (CellulesJeu [i][j] != null){
+                if (CellulesJeu [i][j].jetonCourant != null){
                     continue;
                 }else{
-                    return false; // Ca finit directement la methode?
+                    return false; // Ca finit directement la methode? oui
                 }
             }
         }
-        return true; // Ca return true même si ça a déja return le false avant?
+        return true; // Ca return true même si ça a déja return le false avant? non
     }
     
     public void viderGrille(){
         for (int i = 0; i < 6; i++){
             for (int j = 0; j < 7; j++){
-                CellulesJeu [i][j] = null;
+                CellulesJeu [i][j].jetonCourant = null;
             }
         }
     }
@@ -63,9 +61,24 @@ public class Grille {
         for (int i = 0; i < 6; i++){
             System.out.println("");
             for (int j = 0; j < 7; j++){
-                System.out.print(CellulesJeu [i][j] + " ");
+                System.out.print(CellulesJeu [i][j].jetonCourant + " ");
             }
         }
     }
+    
+    
+    public boolean colonneRemplie(int colonne){
+        int c = colonne;
+        for (int i =0; i < 6; i++){
+            if (CellulesJeu [i][c].jetonCourant == null){
+                return false;
+            }   
+        }
+        return true;
+        
+    }
+    
+    
+    
     
 }
