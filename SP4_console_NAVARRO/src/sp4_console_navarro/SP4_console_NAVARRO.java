280c94f3384f8c4a796a8d45c40e08158f7eb99c
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-11-17 11:42:36 +0100

    SP4

--- SP4_console_NAVARRO/src/sp4_console_navarro/SP4_console_NAVARRO.java
+++ SP4_console_NAVARRO/src/sp4_console_navarro/SP4_console_NAVARRO.java
@@ -11,7 +11,14 @@ public class SP4_console_NAVARRO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Joueur joueur1 = new Joueur("joueur1");
+        Joueur joueur2 = new Joueur("joueur2");
+        
+        
+        
+        
+        
     }
     
 }
