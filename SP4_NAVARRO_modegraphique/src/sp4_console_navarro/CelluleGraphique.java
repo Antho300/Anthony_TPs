d6cf1d990c4f4b383ddb544c5f6b4852938d848d
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-11-20 20:21:27 +0100

    SP4_graphique

--- SP4_NAVARRO_modegraphique/src/sp4_console_navarro/CelluleGraphique.java
+++ SP4_NAVARRO_modegraphique/src/sp4_console_navarro/CelluleGraphique.java
@@ -4,6 +4,8 @@
 
 package sp4_console_navarro;
 
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 /**
@@ -11,14 +13,19 @@ import javax.swing.JButton;
  * @author Anthony
  */
 public class CelluleGraphique extends JButton {
-    Cellule celluleAssociee; // 10'52.
+    Cellule celluleAssociee;
+    ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/Images/celluleVide.png"));
     
     public CelluleGraphique (Cellule uneCellule){
         celluleAssociee = uneCellule;
          
     }
     
-    
+    @Override
+    public void paintComponent (Graphics G){
+        super.paintComponent(G);
+        setIcon(img_vide); // On attribue l'image celluleVide.png
+    }
     
     
 }
