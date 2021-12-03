c439c763e9eaf19995afd8016d74f93d5ccbe64a
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-12-03 15:09:53 +0100

    Mastermind_création du tableau de couleurs

--- Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/JetonsCouleurs.java
+++ Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/JetonsCouleurs.java
@@ -8,4 +8,15 @@ package mastermind_navarro_anthony;
  */
 public class JetonsCouleurs {
     
+    public JetonsCouleurs(){
+        int toutesCouleurs [] = {1,2,3,4,5,6} ;
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 }
