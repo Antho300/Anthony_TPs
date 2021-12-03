25b23f7e8717b1beee5976e2aeedcc59dc35ad52
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-12-03 15:19:34 +0100

    Mastermind_création de la methode CreerLigneReference

--- Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/JetonsCouleurs.java
+++ Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/JetonsCouleurs.java
@@ -2,6 +2,8 @@
         
 package mastermind_navarro_anthony;
 
+import java.util.Random;
+
 /**
  *
  * @author Anthony
@@ -9,8 +11,16 @@ package mastermind_navarro_anthony;
 public class JetonsCouleurs {
     
     public JetonsCouleurs(){
-        int toutesCouleurs [] = {1,2,3,4,5,6} ;
+        int toutesCouleurs [] = {0,1,2,3,4,5}; // Tableau de couleurs.
         
+        
+    }
+    
+    public void CreerLigneReference(){
+        Random alea = new Random();
+        
+        
+        int couleur = alea.nextInt(6);
         
     }
     
