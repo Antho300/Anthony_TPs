59b77ea9b5db4f7afd0b08a6d662333370f4fa23
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-12-23 15:20:28 +0100

    Fonction "initialiserpartie" modif et finie

--- Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Partie.java
+++ Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Partie.java
@@ -8,17 +8,20 @@ import java.util.Scanner;
  * @author Anthony
  */
 public class Partie {
-    Grille grillejeu[][] = new Grille[12][4];
+    Grille grillejeu;
+    String nomjoueur;
     int mode = 0;
     
     void initialiserpartie(){
+        grillejeu = new Grille();
+        
         Scanner sc = new Scanner(System.in);
         int bon = 0;
         int modetemporaire = 0;
         System.out.println("___________________MASTERMIND by Matthieu and Anthony___________________");
         System.out.println("Bonjour !\nBienvenue sur le Mastermind imaginé par Matthieu et Anthony");
         System.out.println("Entrez votre nom :");
-        String nomjoueur = sc.next();
+        nomjoueur = sc.next();
         System.out.println(" ");
         System.out.println("A vous de jouer " + nomjoueur + " !");
         System.out.println("Il existe 3 modes de jeu :\n1- Le mode 'Simple' (Aligne les 3 couleurs dans l'ordre imaginé par l'ordinateur en moins de 12 essais, tu as le choix entre 4 couleurs différentes)\n2- Le mode 'Normal' (Aligne 4 couleurs en moins de 12 essais, tu as le choix entre 4 couleurs différentes)\n3- Le mode 'Extrême' (Aligne 5 couleurs en moins de 15 essis,tu as le choix entre 6 couleurs différentes)");
@@ -45,6 +48,16 @@ public class Partie {
         }
     
     }
+    
+    public void debuterpartie (){
+        System.out.println("Allons ordinateur, initialise une suite de couleurs pour voir de quoi" + nomjoueur + "est capable");
+        
+        
+        
+        
+    }
+    
+    
     
 }
 
