d2a760c93c36622ed9e5b9996798e3d39c73d6ce
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-12-23 10:55:47 +0100

    Mise à jour de la classe grille avec l'avancée de Matthieu

--- Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Boule.java
+++ Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Boule.java
@@ -20,4 +20,3 @@ public class Boule {
         return Couleur;
     }
 }
-
--- Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Mastermind_NAVARRO_Anthony.java
+++ Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Mastermind_NAVARRO_Anthony.java
@@ -12,7 +12,13 @@ public class Mastermind_NAVARRO_Anthony {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Grille gri = new Grille();
+        gri.creercombinaison();
+        gri.affichercombinaison();
+   
+    
+}
+
         
         
         
