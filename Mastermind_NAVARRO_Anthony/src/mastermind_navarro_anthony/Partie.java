cf4293cddeda286a1ca23aff2de48685c9666bf1
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-12-23 10:53:01 +0100

    Ajout de la classe partie

---
+++ Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Partie.java
@@ -0,0 +1,21 @@
+
+package mastermind_navarro_anthony;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Partie {
+    
+    public class Partie {
+    Grille grillejeu[][] = new Grille[12][4];
+    
+    void initialiserpartie(){
+    }
+}
+
+    
+    
+    
+    
+}
