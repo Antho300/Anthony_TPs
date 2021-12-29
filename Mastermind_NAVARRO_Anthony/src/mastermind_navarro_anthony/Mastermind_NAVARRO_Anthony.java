98465aad240f51663203ce754d24fed45c0b48c5
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-12-29 09:48:27 +0100

    Enregistrement du gagant avec le nombre de tours avant la victoire dans un fichier txt

--- Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Mastermind_NAVARRO_Anthony.java
+++ Mastermind_NAVARRO_Anthony/src/mastermind_navarro_anthony/Mastermind_NAVARRO_Anthony.java
@@ -2,6 +2,8 @@
 
 package mastermind_navarro_anthony;
 
+import java.io.FileNotFoundException;
+
 /**
  *
  * @author Anthony
@@ -11,7 +13,7 @@ public class Mastermind_NAVARRO_Anthony {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
      Partie partie = new Partie();
      
      partie.initialiserpartie();
